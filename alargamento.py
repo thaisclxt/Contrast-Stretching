1817adf7bcd76d2b9e9442c016806a643c565e17
Thaís <thais.calixto@mail.uft.edu.br>
2021-05-15 18:10:21 -0300

    prints para eu entender oq tá rolando

--- alargamento.py
+++ alargamento.py
@@ -4,10 +4,11 @@ def contrast_stretching(row, column):
     smaller_range_value = 0
     larger_range_value = 7
 
-    image = np.array([[0] * row] * column)
+    image = [[0] * row] * column
     for x in range(row):
         for y in range(column):
-            image[x][y] = np.array(int(input()))
+            image[x][y] = int(input())
+            print("image[{0}][{1}] = {2}", x, y, image[x][y])
 
     a = np.array(image).flatten()
     smaller_image_value = min(a)
@@ -16,6 +17,7 @@ def contrast_stretching(row, column):
     new_image = [[0] * row] * column
     for x in range(row):
         for y in range(column):
+            print("({0} - {1}) * (({2} - {3}) / ({4} - {1}) + {3}) ".format(image[x][y], smaller_image_value, larger_range_value, smaller_range_value, larger_image_value))
             new_image[x][y] = (image[x][y] - smaller_image_value) * ((larger_range_value - smaller_range_value) / (larger_image_value - smaller_image_value)) + smaller_range_value
 
     print(new_image)
