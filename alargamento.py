aefc6197e61d45f6066e701a5ad4eecde6ad6ba7
Thaís <thais.calixto@mail.uft.edu.br>
2021-05-15 17:20:43 -0300

    Alargamento

---
+++ alargamento.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def contrast_stretching(row, column):
+    smaller_range_value = 0
+    larger_range_value = 7
+
+    image = np.array([[0] * row] * column)
+    for x in range(row):
+        for y in range(column):
+            image[x][y] = np.array(int(input()))
+
+    a = np.array(image).flatten()
+    smaller_image_value = min(a)
+    larger_image_value = max(a)
+
+
+    new_image = [[0] * row] * column
+    for x in range(row):
+        for y in range(column):
+            new_image[x][y] = (image[x][y] - smaller_image_value) * ((larger_range_value - smaller_range_value) / (larger_image_value - smaller_image_value)) + smaller_range_value
+
+    print("::::::")
+    for x in range(row):
+        for y in range(column):
+            print(new_image[x][y])
+
+row = int(input("Entre com a quantidade de linhas: "))
+column = int(input("Entre com a quantidade de colunas: "))
+
+contrast_stretching(row, column)
