58971705d713e781a9509b85bdd4384eac9a52ea
Thaís <thais.calixto@mail.uft.edu.br>
2021-05-15 17:33:49 -0300

    Melhor visualização da matriz

--- alargamento.py
+++ alargamento.py
@@ -13,16 +13,12 @@ def contrast_stretching(row, column):
     smaller_image_value = min(a)
     larger_image_value = max(a)
 
-
     new_image = [[0] * row] * column
     for x in range(row):
         for y in range(column):
             new_image[x][y] = (image[x][y] - smaller_image_value) * ((larger_range_value - smaller_range_value) / (larger_image_value - smaller_image_value)) + smaller_range_value
 
-    print("::::::")
-    for x in range(row):
-        for y in range(column):
-            print(new_image[x][y])
+    print(new_image)
 
 row = int(input("Entre com a quantidade de linhas: "))
 column = int(input("Entre com a quantidade de colunas: "))
